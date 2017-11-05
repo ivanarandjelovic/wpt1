8e8df2e09cb42098a08ba8f3712c6208f8c34593
Ivan Arandjelovic <ivan.arandjelovic@gmail.com>
2017-11-05 22:11:57 +0100

    react works

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-var _=require("lodash");
+import React from "react";
+import ReactDOM from "react-dom";
+import Hello from "./Hello";
 
-function component() {
-  var element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+ReactDOM.render(
+  <Hello name="World" />,
+  document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,29 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: "/dist"
-  }
+  },
+  module: {
+  //  preLoaders: [
+  //     {
+  //       test: /\.js$/,
+  //       exclude: /node_modules/,
+  //       loader: 'jshint-loader'
+   //
+  //     }
+  //  ],
+   loaders: [
+     {
+       test: [/\.js$/, /\.jsx$/],
+       exclude: /node_modules/,
+       loader: 'babel-loader',
+       query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015']
+       }
+      }
+   ]
+ },
+ resolve: {
+   extensions: ['.js', '.jsx']
+ }
 };
